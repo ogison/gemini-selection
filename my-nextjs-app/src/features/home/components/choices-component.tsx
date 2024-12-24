c22f980070661d8735377e35d066800756bb8465
ogison <meyo.ogs9@gmail.com>
2024-12-25 00:23:24 +0900

    Added: the function changing the order

--- my-nextjs-app/src/features/home/components/choices-component.tsx
+++ my-nextjs-app/src/features/home/components/choices-component.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+import { DndContext } from '@dnd-kit/core';
+import { SortableContext } from '@dnd-kit/sortable';
+import { FormLabel } from '@/components/ui/form';
 import { Item } from '../types';
-import { Button } from '@/components/ui/button';
-import { Trash2 } from 'lucide-react';
+import DraggableRow from './choices-row';
 
 interface Props {
   fields: Item[];
@@ -15,68 +14,21 @@ interface Props {
 export const ChoicesComponent = (props: Props) => {
   const { fields, remove, move } = props;
 
-  // ドラッグ終了時に順番を更新する
   const handleDragEnd = (result: any) => {
-    console.log('aaaaa', result);
-    if (!result.destination) return;
-    move(result.source.index, result.destination.index);
+    if (!result.over) return;
+    move(result.active.data.current.sortable.index, result.over.data.current.sortable.index);
   };
 
   return (
     <>
       <FormLabel>選択肢（必須）：</FormLabel>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="fields" isDropDisabled={false} isCombineEnabled={false} ignoreContainerClipping={false}>
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps} className="relative overflow-visible">
-              {fields.map((item, index) => (
-                <Draggable key={item.id} draggableId={String(item.id)} index={index}>
-                  {(provided) => (
-                    <div ref={provided.innerRef} {...provided.draggableProps} style={provided.draggableProps.style}>
-                      <FormField
-                        name={`items.${index}.value`}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormControl>
-                              <div className="flex items-center space-x-2">
-                                <div {...provided.dragHandleProps} className="cursor-grab">
-                                  <span>☰</span>
-                                </div>
-                                <Input
-                                  aria-label={`選択肢 ${index + 1}`}
-                                  className="grow"
-                                  onChange={(e) => {
-                                    field.onChange(e.target.value), console.log(fields);
-                                  }}
-                                  placeholder={`選択肢 ${index + 1}`}
-                                  type="text"
-                                  value={field.value}
-                                />
-                                <Button
-                                  aria-label={`選択肢 ${index + 1} を削除`}
-                                  className="shrink-0"
-                                  onClick={() => remove(index)}
-                                  size="icon"
-                                  type="button"
-                                  variant="destructive"
-                                >
-                                  <Trash2 className="size-4" />
-                                </Button>
-                              </div>
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <DndContext onDragEnd={handleDragEnd}>
+        <SortableContext items={fields}>
+          {fields.map((item, index) => (
+            <DraggableRow key={item.id} item={item} id={item.id.toString()} index={index} remove={remove} />
+          ))}
+        </SortableContext>
+      </DndContext>
     </>
   );
 };
---
+++ my-nextjs-app/src/features/home/components/choices-row.tsx
@@ -0,0 +1,65 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { Item } from '../types';
+import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Trash2 } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+
+interface Props {
+  item: Item;
+  id: string;
+  index: number;
+  remove: (index: number | number[]) => void;
+}
+
+const DraggableRow = (props: Props) => {
+  const { item, id, index, remove } = props;
+  const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({ id });
+
+  return (
+    <div
+      key={id}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition,
+      }}
+    >
+      <FormField
+        name={`items.${index}.value`}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <div className="flex items-center space-x-2" key={item.id}>
+                <Input
+                  aria-label={`選択肢 ${index + 1}`}
+                  className="grow"
+                  onChange={(e) => field.onChange(e.target.value)}
+                  placeholder={`選択肢 ${index + 1}`}
+                  type="text"
+                  value={field.value}
+                />
+                <Button
+                  aria-label={`選択肢 ${index + 1} を削除`}
+                  className="shrink-0"
+                  onClick={() => remove(index)}
+                  size="icon"
+                  type="button"
+                  variant="destructive"
+                >
+                  <Trash2 className="size-4" />
+                </Button>
+              </div>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default DraggableRow;
